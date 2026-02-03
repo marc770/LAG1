e370e09a73896641a5ff737d9973c931734d602b
Marcus Andersson <marcus.andersson@sr.se>
2026-02-03 20:16:51 +0100

    scenario button

--- src/menu.js
+++ src/menu.js
@@ -1,19 +1,32 @@
 import './style.css'
+import './menu.css'
 import klirrLogo from '/klirr.png'
 
 document.querySelector('#app').innerHTML = `
-  <div>
+  <div class="page">
     <img src="${klirrLogo}" class="logo" alt="Klirr logo" />
     <h1>Meny</h1>
 
-    <div class="card">
-      <button id="btnStart" type="button">Inräkna</button>
+    <div class="menu">
+      <button id="btnStart" class="menu-btn" type="button">
+        <span>Inräkna</span>
+        <span class="chev">›</span>
+      </button>
+
+    <button id="btnIsstorm" class="menu-btn" type="button" disabled aria-disabled="true" title="Kommer snart">
+        <span>Isstorm <small class="tag">(Kommer snart)</small></span>
+        <span class="chev">›</span>
+    </button>
     </div>
 
-    <p style="opacity:.7">More buttons will go here later.</p>
+    <p class="muted">More buttons will go here later.</p>
   </div>
 `
 
 document.getElementById('btnStart').addEventListener('click', () => {
   window.location.href = '/app.html'
+})
+
+document.getElementById('btnIsstorm').addEventListener('click', () => {
+  window.location.href = '/isstorm.html'
 })
