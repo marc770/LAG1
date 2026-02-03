170a3895c4ae1ac375007b56496f6dcd8a1a364c
Marcus Andersson <marcus.andersson@sr.se>
2026-02-03 10:42:51 +0100

    pwa plugin

---
+++ vite.config.js
@@ -0,0 +1,36 @@
+// vite.config.js
+import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
+
+export default defineConfig({
+  // For a user site at https://<user>.github.io/, either omit base or set '/'
+  base: '/',
+  build: { outDir: 'docs' },
+  plugins: [
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: [
+        'icons/icon-192.png',
+        'icons/icon-512.png'
+      ],
+      manifest: {
+        name: 'HelloPWA',
+        short_name: 'HelloPWA',
+        start_url: '/',
+        scope: '/',
+        display: 'standalone',
+        background_color: '#111111',
+        theme_color: '#111111',
+        description: 'A lightweight audio visualizer PWA built with Vite + Lit.',
+        icons: [
+          { src: '/icons/icon-192.png', sizes: '192x192', type: 'image/png' },
+          { src: '/icons/icon-512.png', sizes: '512x512', type: 'image/png' }
+          // If you have maskables, include them too:
+          // { src: '/icons/maskable-192.png', sizes: '192x192', type: 'image/png', purpose: 'maskable' },
+          // { src: '/icons/maskable-512.png', sizes: '512x512', type: 'image/png', purpose: 'maskable' }
+        ]
+      },
+      devOptions: { enabled: false } // use prod SW for preview
+    })
+  ]
+})
